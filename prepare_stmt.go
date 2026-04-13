4f42640fd01772c7b7db34a184b4683f2afebe0c
María Florencia Caballero <66144664+MFCaballero@users.noreply.github.com>
2026-04-13 18:11:16 +0200

    feat(PreparedStmtDB): support reset (#5782)
    Cherry-picked from upstream go-gorm/gorm commit 5dd2bb4.
    Adds Reset() method to PreparedStmtDB, needed by
    gorm.io/driver/postgres v1.5.0.

--- prepare_stmt.go
+++ prepare_stmt.go
@@ -42,6 +42,18 @@ func (db *PreparedStmtDB) Close() {
 	db.Mux.Unlock()
 }
 
+func (db *PreparedStmtDB) Reset() {
+	db.Mux.Lock()
+	defer db.Mux.Unlock()
+	for query, stmt := range db.Stmts {
+		delete(db.Stmts, query)
+		go stmt.Close()
+	}
+
+	db.PreparedSQL = make([]string, 0, 100)
+	db.Stmts = map[string]Stmt{}
+}
+
 func (db *PreparedStmtDB) prepare(ctx context.Context, conn ConnPool, isTransaction bool, query string) (Stmt, error) {
 	db.Mux.RLock()
 	if stmt, ok := db.Stmts[query]; ok && (!stmt.Transaction || isTransaction) {
